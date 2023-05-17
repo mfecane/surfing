59a1e25b8bdb46074120fd8e9a6b02f6c2ecbfb2
Aleksei Aliapkin <aleksei.aliapkin@planner5d.com>
2023-05-17 21:05:01 +0300

    add water collision simulation

--- scripts/WaterSimulation.gd
+++ scripts/WaterSimulation.gd
@@ -1,14 +1,28 @@
 extends Node3D
 
+@onready var physics: Physics = $"/root/world/Physics"
 @onready var cr = $Simulation/ColorRect
 @onready var water = $Water
+
+var simulationRectMaterial
+var waterMaterial
 
 func _ready():
 	var simulationTexture = $Simulation.get_texture()
 	var collisionTexture = $Collision.get_texture()
 	
-	cr.material.set_shader_parameter('sim_tex', simulationTexture)
-	cr.material.set_shader_parameter('col_tex', collisionTexture)
+	
+	simulationRectMaterial = cr.material
+	simulationRectMaterial.set_shader_parameter('simultationTexture', simulationTexture)
+	simulationRectMaterial.set_shader_parameter('collisionTexture', collisionTexture)
 
-	water.mesh.surface_get_material(0).set_shader_parameter('simulationTexture', simulationTexture)
+	waterMaterial = water.mesh.surface_get_material(0)
+
+	waterMaterial.set_shader_parameter('simulationTexture', simulationTexture)
 	#water.mesh.surface_get_material(0).set_shader_parameter('simulation2', simulationTexture)
+
+func _process(delta):
+	var boardSpeed = physics.boardKinematics.boardSpeed
+	var boardPosition = physics.boardKinematics.boardPosition
+	simulationRectMaterial.set_shader_parameter('shift', Vector2(boardSpeed.x * 0.01, boardSpeed.z * 0.01))
+	waterMaterial.set_shader_parameter("shift", Vector2(boardPosition.x, boardPosition.z))
